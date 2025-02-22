1d47b163fc6e4e2ed1b6d4fcf42e196ff9293a2b
kahput <kahputs@gmail.com>
2025-02-22 11:38:40 +0100

    feat: create react-fact static website

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <img src="react.svg" alt="React logo" />
+        <span>ReactFacts</span>
+
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar;
